eb165d6aecc00f907620e1f0a4c62c7b6783c5b4
Mark Ochieng Oduor <markochieng5577@gmail.com>
2026-08-29 22:14:52 +0300

    fix(deployment): include website seeders and configure safe incremental migrations on render

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,10 +9,18 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            RolePermissionSeeder::class,
+            PackageSeeder::class,
             MultiSchoolTenantSeeder::class,
             AcademicAndStudentRelationalSeeder::class,
             FinancialAndHRSeeder::class,
             AssessmentCoCurricularAndLogisticsSeeder::class,
+            WebsiteSeeder::class,
+            WebsitePageSeeder::class,
+            WebsiteAboutSeeder::class,
+            WebsiteLegalSeeder::class,
+            BlogPostSeeder::class,
+            FaqSeeder::class,
         ]);
     }
 }
